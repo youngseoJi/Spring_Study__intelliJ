afbe677736f7cc77787e74ad05049c7a5d0b1aef
youngseoJi <estell.ys.j@gmail.com>
2022-09-15 17:48:49 +0900

    inteliJ: 싱글톤 컨테이너 (스프링컨테이너) - 스프링 컨테이너를 사용하는 테스트 코드 적용하기 (싱글톤 컨테이너 해결방안)

--- inteliJ-1/src/test/java/hello/core/singleton/SingletonTest.java
+++ inteliJ-1/src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 /**       스프링 없는 순수한 DI 컨테이너인 AppConfig는 요청을 할 때 마다 객체를 새로 생성한다.
@@ -37,12 +39,27 @@ public class SingletonTest {
         // 있는 객체 SingleTonService 를 활용, 새롭게 생성 X 호출할 때 마다 같은 객체 인스턴스를 반환하는 것을 확인할 수 있다.
         SingleTonService singletonService1 = SingleTonService.getInstance();
         SingleTonService singletonService2 = SingleTonService.getInstance();
-        //테스트 확인 : 두 싱글톤서비스는 달라야 함  singletonService1 !==  singletonService2
+        //테스트 확인 : 참조값이 같은 것을 확인
         System.out.println("singletonService1 = " + singletonService1);
         System.out.println("singletonService2 = " + singletonService2);
-
+        // 테스트 확인 : 두 싱글톤서비스는 같아야 함 singletonService1 == singletonService2
         assertThat(singletonService1).isSameAs(singletonService2);
         // same == 참조를 비교, 참조객체, 인스턴스 비교 완전히 같은지
         // equal 이퀄즈 비교하는것
+    }
+
+    // 스프링 컨테이너 덕분에 고객의 요청이 올 때 마다 객체를 생성하는 것이 아니라, 이미 만들어진 객체를 공유해서 효율적으로 재사용할 수 있다.
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //테스트 확인 : 참조값이 같은 것을 확인
+        System.out.println("MemberService1 = " + memberService1);
+        System.out.println("MemberService2 = " + memberService2);
+        // 테스트 확인 : 두 싱글톤서비스는 같아야 함 singletonService1 == singletonService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
