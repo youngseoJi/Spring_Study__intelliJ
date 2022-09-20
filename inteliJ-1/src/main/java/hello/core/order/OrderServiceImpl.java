72adb62f159efe83865bf744592eaaa775d9d3e2
youngseoJi <estell.ys.j@gmail.com>
2022-09-20 15:20:42 +0900

    다양한 의존관계 주입 방법 4가지 : 생성자, 수정자 주입위주 !

--- inteliJ-1/src/main/java/hello/core/order/OrderServiceImpl.java
+++ inteliJ-1/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,19 +10,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 // 객체지향
-// 주문
+
+// 생성자 주입은 빈이 등록될떄 같이 주입된다. // OrderServiceImpl 생성 빈에 등록하면서 자동주입
 @Component // 컴포넌트 스캔의 대상이 되면서 스프링빈에 등록된다.
 public class OrderServiceImpl implements OrderService{
 
     // DIP, 준수하기 - 인테페이스에만 의존하게 변경한 것
+    // 불변 의존관계
     private final MemberRepository memberRepository; // 회원저장소 인터페이스
     private final DiscountPolicy discountPolicy; // 할인정책 인터페이스
     //  DIP, 준수안한 것 =>  인터페이스에만 의존하도록 클래스 구현체 X, 관심사 분리! 인터페이스와 구현체 분리
     //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 정액할인(고정할인) 정책
     //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 정률할인 정책
 
-    @Autowired // 생성자 위에 작성하면, 스프링이 이것을 생성할때 자동으로 MemberRepository, DiscountPolicy 모두 주입해준다.
+//    // 수정자 주입(setter 주입)
+//    @Autowired // 의존관계 자동주입
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        System.out.println("memberRepository = " + memberRepository);
+//        this.memberRepository = memberRepository;
+//    }
+//    @Autowired // 의존관계 자동주입
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        System.out.println("discountPolicy = " + discountPolicy);
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    // OrderServiceImpl 생성자 생성
+    //  @Autowired // 생성자 위에 Autowired를 작성하면, 스프링이 이것을 생성할때 자동으로 MemberRepository, DiscountPolicy 모두 주입해준다.
+    //생성자가 딱 1개일때는 Autowired가 자동 적용되므로안적어도 된다.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        //System.out.println("첫번째 액션, 출력 : memberRepository = " + memberRepository);
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
--- inteliJ-1/src/test/java/hello/core/CoreApplicationTests.java
+++ inteliJ-1/src/test/java/hello/core/CoreApplicationTests.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest // 스프링이 자동으로 스프링 컨테이너를 띄워서 테스트를 통합해서 테스트를 실행해준다.
 class CoreApplicationTests {
 
 	@Test
