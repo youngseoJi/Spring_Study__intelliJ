d241ae90d152fed2c4cb8fa91fd20212f04b65cc
youngseoJi <estell.ys.j@gmail.com>
2022-09-20 10:54:09 +0900

    @ComponentScan, @Component : 중복 등록과 충돌  수동 빈 등록 vs 자동 빈 등록

--- inteliJ-1/src/main/java/hello/core/AutoAppConfig.java
+++ inteliJ-1/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -17,4 +20,8 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
