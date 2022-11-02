0c3e11a7da7e66fac55934dc4c4187ccccad6e27
youngseoJi <estell.ys.j@gmail.com>
2022-11-02 13:40:42 +0900

    Http 응답데이터 전달방법 : API JSON으로 응답에이터를 전달하는 방법

---
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,32 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("utf-8");
+
+        HelloData data = new HelloData();
+        data.setUsername("Ji");
+        data.setAge(29);
+        // json 형태로 변경 하기 위해 ObjectMapper 사용하기 -> 객체 형태로 변형 {"" : "",..}
+        // ObjectMapper를 이용하면 JSON을 Java 객체로 변환할 수 있고, 반대로 Java 객체를 JSON 객체로 serialization 할 수 있다.
+        String result = objectMapper.writeValueAsString(data);
+
+        resp.getWriter().write(result);
+    }
+}
