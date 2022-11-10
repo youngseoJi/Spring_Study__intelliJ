617e0528590f9f5a0f0ff064fec9792173e41ab7
youngseoJi <estell.ys.j@gmail.com>
2022-11-10 14:11:39 +0900

    스프링 MVC - 핸들러 매핑과 핸들러 어댑터 : 과거에 사용했던 스프링이 제공하는 간단한 컨트롤러로 핸들러 매핑 알아보기 수정

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,8 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동등록
 @SpringBootApplication
--- servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -14,6 +14,7 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest"); // controller 호출성공 확인
-        return null;
+        return new ModelAndView("new-form");
+
     }
 }
