ef69324e089d16ed50dc24da6cc1e169c6132bb8
youngseoJi <estell.ys.j@gmail.com>
2022-11-10 16:23:55 +0900

    스프링 MVC 3 - 실용적인 방식 알아보기 이노테이션의 기능 활용하기

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,51 @@
+package hello.servlet.web.springmvc.v3;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public String newFrom() {
+        return "new-form";
+        // ModelAndView로 반환을 하지않아도 알아서, dp노테이션이 view이름을 반환했음을 알고 프로세스가 진행된다.
+    }
+    /*  public ModelAndView newFrom() {
+        return new ModelAndView("new-form"); */
+
+    @RequestMapping("/save")
+    public String save(
+            // 요청 파라미터 명과, 타입캐스팅도 처리 가능(타로 int 파싱 안해줘도 된다.)
+            @RequestParam("username") String username,
+            @RequestParam("age") int age,
+            Model model) {
+
+        Member member = new Member(username, age);;
+        memberRepository.save(member);
+
+        // model에 가입한 회원정보 담아주기
+        model.addAttribute("member", member);
+        return "save-result"; // view 이름 반환
+    }
+
+    @RequestMapping
+    public String members(Model model) {
+        List<Member> members = memberRepository.findAll(); // 모든 회원 members 배열에 저장
+
+        model.addAttribute("members", members);
+        return "members";
+    }
+}
