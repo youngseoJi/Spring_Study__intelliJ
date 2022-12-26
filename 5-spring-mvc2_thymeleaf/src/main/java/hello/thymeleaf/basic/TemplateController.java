1377d230b25d1debe91597d5a3b18b1ef5f57319
youngseoji <estell.ys.j@gmail.com>
2022-12-26 13:30:27 +0900

    Add : 템플릿 레이아웃 기능 2  - html 레이아웃을 자체를 공통레이아웃을 변경하는  - 공통으로 사용할 레이아웃 지정  - 코드조각 추가하여 부분 값 변경 : ~{::태그}

--- 5-spring-mvc2_thymeleaf/src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ 5-spring-mvc2_thymeleaf/src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -22,4 +22,9 @@ public class TemplateController {
     public String layout() {
         return "template/layout/layoutMain";
     }
+
+   @GetMapping("/layoutExtend")
+    public String layoutExtend() {
+        return "template/layoutExtend/layoutExtendMain";
+   }
 }
